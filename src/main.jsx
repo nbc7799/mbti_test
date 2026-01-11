13333394127e458ef65e748ffcb5d6d82316cd88
Jun <nbc7799@naver.com>
2026-01-11 21:54:00 +0900

    💚 Fix: fix ci build 에러

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import Layout from 'src/Layout.jsx';
+import Layout from './Layout.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
