4f0747978d1ec5ea3943ccb56937a3fcf759c41e
Jun <nbc7799@naver.com>
2026-01-11 21:38:47 +0900

    ✏️ Fix: ci 빌드에러 수정 #7

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import Layout from './layout.jsx';
+import Layout from './layout';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
