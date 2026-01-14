f789bdf9e68717cf8b23bf386a714e97b6214643
Jun <nbc7799@naver.com>
2026-01-14 22:20:28 +0900

    ✨ Feat: intro에 mbti별 각 점수판 생성 #11

--- src/components/test/IntroRenderer.jsx
+++ src/components/test/IntroRenderer.jsx
@@ -4,12 +4,30 @@ import Quiz from './Quiz';
 import Loading from './Loading';
 
 export default function IntroRenderer({ currentTest }) {
+  //점수판
+  const [mbtiScore, setMbtiScore] = useState({
+    E: 0,
+    I: 0,
+    S: 0,
+    N: 0,
+    T: 0,
+    F: 0,
+    J: 0,
+    p: 0,
+  });
+
   const [mode, setMode] = useState('intro');
 
   if (mode === 'intro') {
     return <Intro info={currentTest?.info} setMode={setMode} />;
   } else if (mode === 'quiz') {
-    return <Quiz />;
+    return (
+      <Quiz
+        setMode={setMode}
+        questions={currentTest?.questions}
+        setMbtiScore={setMbtiScore}
+      />
+    );
   } else if (mode === 'loading') {
     return <Loading />;
   } else {
