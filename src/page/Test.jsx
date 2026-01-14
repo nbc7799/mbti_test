8a4c3ed9e99c78a253b107ca7d8de4885f56bf86
Jun <nbc7799@naver.com>
2026-01-14 14:29:18 +0900

    ✨ Feat: 테스트 선택하면 해당된 테스트 찾아서 이동시켜주는 컴포넌트 #9

--- src/page/Test.jsx
+++ src/page/Test.jsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { TESTS } from '../data/TESTS';
+import IntroRenderer from '../components/test/IntroRenderer';
+
 export default function Test() {
-  return <div>테스트 페이지</div>;
+  const { testParam } = useParams();
+  const navigate = useNavigate();
+  const [currentTest, setCurrentTest] = useState();
+
+  useEffect(() => {
+    const theTest = TESTS?.find((test) => test?.info?.mainUrl === testParam);
+
+    if (!theTest) {
+      alert('존재하지 않는 테스트입니다.');
+      return navigate('/');
+    }
+    setCurrentTest(theTest);
+  }, [testParam]);
+
+  return (
+    <div>
+      <IntroRenderer currentTest={currentTest} />
+    </div>
+  );
 }
