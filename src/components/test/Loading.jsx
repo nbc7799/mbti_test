6b6891b3d4be28a9273c559fbe89583cba192016
Jun <nbc7799@naver.com>
2026-01-16 00:45:05 +0900

    ♻️ Refactor: state제거후 useMemo 훅 사용 #13

--- src/components/test/Loading.jsx
+++ src/components/test/Loading.jsx
@@ -1,11 +1,12 @@
 import Lottie from 'react-lottie';
 import * as animationData from '../../assets/loading-animation.json';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Loading({ mbtiScore, currentTest }) {
   const navigate = useNavigate();
-  const loadingTime = 3700; //ms
+  //loading time -> 3.7초(최대치로 이탈률 가장 적음)
+  const loadingTime = 3700;
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -14,11 +15,9 @@ export default function Loading({ mbtiScore, currentTest }) {
       preserveAspectRatio: 'xMidYMid slice',
     },
   };
-  const [finalResultQuery, setFinalResultQuery] = useState('');
 
-  //loading time -> 3.7초(최대치로 이탈률 가장 적음)
-  useEffect(() => {
-    // 4개의 슬롯 [[E,I], [S,N], [T,F], [J,P]]을 돌면서 mbti점수 비교후 결과 도출
+  // 4개의 슬롯 [[E,I], [S,N], [T,F], [J,P]]을 돌면서 mbti점수 비교후 결과 도출
+  const finalResultQuery = useMemo(() => {
     const mbtiPairs = [
       ['E', 'I'],
       ['S', 'N'],
@@ -26,34 +25,33 @@ export default function Loading({ mbtiScore, currentTest }) {
       ['J', 'P'],
     ];
 
-    // 비어있는 문자열
     let resultType = '';
 
     //각 ARRAY 돌면서 -> 각 슬롯의 WINNNER선정 -> 문자열 변수에 추가
     for (let pair of mbtiPairs) {
       let firstType = pair[0];
       let secondType = pair[1];
-      let firstTypeScore = mbtiScore[firstType];
-      let secondTypeScore = mbtiScore[secondType];
-      firstTypeScore > secondTypeScore
-        ? (resultType += firstType)
-        : (resultType += secondType);
+
+      if (mbtiScore[firstType] > mbtiScore[secondType]) {
+        resultType += firstType;
+      } else {
+        resultType += secondType;
+      }
     }
     console.log('resultType', resultType);
 
-    const resultQuery = currentTest?.results?.find((result) => {
+    return currentTest?.results?.find((result) => {
       return result?.type === resultType;
     })?.query;
-    setFinalResultQuery(resultQuery);
-    console.log('resultQuery', resultQuery);
   }, [mbtiScore, currentTest]);
 
   // 결과페이지로 이동
   useEffect(() => {
-    let timeout = setTimeout(() => {
+    const timeout = setTimeout(() => {
       //finalResultQuery 활용해서 결과 페이지로 라우팅
-      finalResultQuery &&
-        navigate(`/${currentTest?.info?.mainUrl}/result/${finalResultQuery}}`);
+      if (finalResultQuery) {
+        navigate(`/${currentTest?.info?.mainUrl}/result/${finalResultQuery}`);
+      }
     }, loadingTime);
 
     return () => {
