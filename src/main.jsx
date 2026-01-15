d4e5ba1d157bec8e35f6d89c8a79937520d62dd9
Jun <nbc7799@naver.com>
2026-01-16 00:31:14 +0900

    ♻️ Refactor: stricmode 제거 #13

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import Layout from './Layout.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Layout>
-      <App />
-    </Layout>
-  </StrictMode>,
+  <Layout>
+    <App />
+  </Layout>,
 );
