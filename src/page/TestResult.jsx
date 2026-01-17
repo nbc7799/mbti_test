f1e218b8bfa825dbf2279e739078731ad38eb8be
Jun <nbc7799@naver.com>
2026-01-17 20:15:55 +0900

    ♻️ Refactor: useMemo사용으로 변경 #15

--- src/page/TestResult.jsx
+++ src/page/TestResult.jsx
@@ -1,39 +1,35 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import ResultButtonGroup from '../components/testResult/ResultButtonGroup';
 import ResultThumbnailList from '../components/testResult/ResultThumbnailList';
 import ShareButtonGroup from '../components/testResult/ShareButtonGroup';
 import TestResultRenderer from '../components/testResult/TestResultRenderer';
-import { useEffect, useState } from 'react';
 import { TESTS } from '../data/TESTS';
+import { useMemo } from 'react';
 
 export default function TestResult() {
-  const navigate = useNavigate();
   const { testParam, resultParam } = useParams();
-  const [renderResultInfo, setRenderResultInfo] = useState({});
 
-  useEffect(() => {
-    const testInfo = TESTS.find((test) => test?.info?.mainUrl === testParam);
+  const testInfo = useMemo(
+    () => TESTS?.find((test) => test?.info?.mainUrl === testParam),
+    [testParam],
+  );
 
-    if (!testInfo) {
-      alert('Invalid testParam');
-      navigate('/');
-    }
+  const resultInfo = useMemo(
+    () => testInfo?.results?.find((result) => result.query === resultParam),
+    [testInfo, resultParam],
+  );
 
-    const resultInfo = testInfo?.results?.find(
-      (result) => result.query === resultParam,
-    );
+  if (!testInfo) {
+    return <Navigate to="/" replace />;
+  }
 
-    if (!resultInfo) {
-      alert('Invalid resultParam');
-      navigate(`/${testInfo?.info?.mainUrl}`);
-    }
-
-    setRenderResultInfo(resultInfo);
-  }, [testParam, resultParam]);
+  if (!resultInfo) {
+    return <Navigate to={`/${testInfo?.info?.mainUrl}`} replace />;
+  }
 
   return (
     <div>
-      <TestResultRenderer renderResultInfo={renderResultInfo} />
+      <TestResultRenderer renderResultInfo={resultInfo} />
       <ShareButtonGroup />
       <ResultButtonGroup />
       <ResultThumbnailList />
@@ -42,4 +38,5 @@ export default function TestResult() {
 }
 
 // 1. Intro-> quiz -> loading -> result
-// 2. Direct Access to result page(공유할경우 아마 이 경우가 더 많은것)
+// 2. Direct Access to result page(공유할경우는 결과페이지가 바로 보이기에
+//  아마 이 경우가 더 많을것)
