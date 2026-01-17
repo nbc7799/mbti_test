ed97ff2109c79a29b3f142b196a9ae8b0bbdc41c
Jun <nbc7799@naver.com>
2026-01-18 01:17:13 +0900

    ✨ Feat: url 카피기능 추가 #19

--- src/components/testResult/ShareButtonGroup.jsx
+++ src/components/testResult/ShareButtonGroup.jsx
@@ -5,24 +5,44 @@ import {
   TwitterShareButton,
 } from 'react-share';
 import { baseURL } from '../../App';
+import styles from './shareButtonGroup.module.css';
 
 export default function ShareButtonGroup({ testParam, resultParam, testInfo }) {
+  const handleCopy = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert('복사되었습니다!');
+    } catch (err) {
+      console.error('복사 실패:', err);
+    }
+  };
+
   return (
-    <div>
+    <div className={styles.shareContainer}>
       <h3>친구에게 공유하기</h3>
-      <FacebookShareButton
-        url={`${baseURL}/${testParam}/result/${resultParam}`}
-        hashtag={`#${testInfo?.info.mainTitle}`}
-      >
-        <FacebookIcon size={48} round={true} />
-      </FacebookShareButton>
-      <TwitterShareButton
-        title={testInfo?.info.mainTitle}
-        url={`${baseURL}/${testParam}/result/${resultParam}`}
-        hashtags={[testInfo?.info.mainTitle]}
-      >
-        <TwitterIcon size={48} round={true} />
-      </TwitterShareButton>
+      <section className={styles.snsBtns}>
+        <FacebookShareButton
+          url={`${baseURL}/${testParam}/result/${resultParam}`}
+          hashtag={`#${testInfo?.info.mainTitle}`}
+        >
+          <FacebookIcon size={48} round={true} />
+        </FacebookShareButton>
+        <TwitterShareButton
+          title={testInfo?.info.mainTitle}
+          url={`${baseURL}/${testParam}/result/${resultParam}`}
+          hashtags={[testInfo?.info.mainTitle]}
+        >
+          <TwitterIcon size={48} round={true} />
+        </TwitterShareButton>
+        <button
+          className={styles.urlShareButton}
+          onClick={() => {
+            handleCopy(`${baseURL}/${testParam}/result/${resultParam}`);
+          }}
+        >
+          URL
+        </button>
+      </section>
     </div>
   );
 }
