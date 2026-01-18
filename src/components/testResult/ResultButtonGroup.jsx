f1ce7f077e5ba574694c02eb5ea4286642b6db39
Jun <nbc7799@naver.com>
2026-01-18 22:53:22 +0900

    ✨ Feat: 결과 버튼 그룹 컴포넌트 완성 #21

--- src/components/testResult/ResultButtonGroup.jsx
+++ src/components/testResult/ResultButtonGroup.jsx
@@ -1,3 +1,49 @@
-export default function ResultButtonGroup() {
-  return <div>ResultButtonGroup</div>;
+import { useNavigate } from 'react-router-dom';
+import { linkCopy } from '../../tools/tools';
+import { HomeOutlined, LinkOutlined, RedoOutlined } from '@ant-design/icons';
+import styles from './resultButtonGroup.module.css';
+
+export default function ResultButtonGroup({ testParam }) {
+  const navigate = useNavigate();
+
+  const onClickRedoButton = () => {
+    navigate(`/${testParam}`);
+  };
+
+  const onClickGoHomeButton = () => {
+    navigate('/');
+  };
+  return (
+    <div className={styles.mainDiv}>
+      <div className={styles.upperDiv}>
+        <button
+          className={styles.upperButton}
+          onClick={() => {
+            linkCopy();
+          }}
+        >
+          <LinkOutlined />
+          링크 복사
+        </button>
+        <button
+          className={styles.upperButton}
+          onClick={() => {
+            onClickRedoButton();
+          }}
+        >
+          <RedoOutlined />
+          다시 하기
+        </button>
+      </div>
+      <button
+        className={styles.bottomButton}
+        onClick={() => {
+          onClickGoHomeButton();
+        }}
+      >
+        <HomeOutlined />
+        다른 테스트 하러 가기
+      </button>
+    </div>
+  );
 }
