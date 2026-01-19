4d342c24403603a0ea6776eca727874696a267c9
Jun <nbc7799@naver.com>
2026-01-19 22:13:46 +0900

    ✨ Feat: 테스트결과 다른 섬네일 리스트들 보여주기 #23

--- src/components/testResult/ResultThumbnailList.jsx
+++ src/components/testResult/ResultThumbnailList.jsx
@@ -1,3 +1,23 @@
-export default function ResultThumbnailList() {
-  return <div>ResultThumbnailList</div>;
+import { useMemo } from 'react';
+import { TESTS } from '../../data/TESTS';
+import { Link } from 'react-router-dom';
+
+export default function ResultThumbnailList({ testParam }) {
+  const otherTests = useMemo(() => {
+    return TESTS.filter((test) => test?.info?.mainUrl !== testParam);
+  }, [testParam]);
+
+  return (
+    <div>
+      {otherTests.map((item) => (
+        <Link to={`/${item?.info?.mainUrl}`} key={item?.info?.mainUrl}>
+          <img
+            style={{ width: '100%' }}
+            src={item?.info?.thumbImage}
+            alt={item?.info?.mainTitle}
+          />
+        </Link>
+      ))}
+    </div>
+  );
 }
